c6bb1b53fc4bb70b4e796aca05b87b1091d5046f
fabiano <fabiano.pavan@soluzionipa.it>
2018-07-17 17:28:50 +0200

    Corretto require su xml_security

--- lib/xml_security.rb
+++ lib/xml_security.rb
@@ -29,7 +29,7 @@ require "openssl"
 require 'nokogiri'
 require "digest/sha1"
 require "digest/sha2"
-require "Federa/ruby-saml/validation_error"
+require "federa/ruby-saml/validation_error"
 
 module XMLSecurity
 
